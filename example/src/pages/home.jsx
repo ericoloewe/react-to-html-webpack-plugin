ec583af13a2fcfa2d4bec92f1dcd14cc2f51d87d
Érico de Souza Loewe <erico.loewe@cwi.com.br>
2018-03-21 10:27:20 -0300

    feat(home): add script and improve attrs

--- example/src/pages/home.jsx
+++ example/src/pages/home.jsx
@@ -5,13 +5,14 @@ export default class Home extends React.Component {
     return (
       <html lang="en">
         <head>
-          <meta charset="UTF-8" />
+          <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <title>Lorem ipsum</title>
         </head>
         <body>
           <h1>First test :D</h1>
+          <script src="./containers.js"></script>
         </body>
       </html>
     );
