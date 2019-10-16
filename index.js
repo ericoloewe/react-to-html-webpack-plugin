e11c69a20590fd0626a8bba9ceaa9d1c9fe596c5
Érico de Souza Loewe <erico.loewe@cwi.com.br>
2019-10-16 18:08:20 -0300

    feat(cache): add for source compilations

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-const ReactDOMServer = require("react-dom/server");
-const React = require("react");
-const evaluate = require("eval");
+const ReactDOMServer = require('react-dom/server');
+const React = require('react');
+const evaluate = require('eval');
+
+const { getDataFromCacheOrMethod } = require('./cache');
 
 const GLOBALS_MOCK = { global, window: global };
 
-module.exports = class ReactToHtmlWebpackPlugin {
+module.exports = class ReactToStaticHtmlWebpackPlugin {
   constructor(props = {}) {
     this.globals = Object.assign(GLOBALS_MOCK, props.globals) || GLOBALS_MOCK;
-    this.htmlHeader = props.htmlHeader || "<!DOCTYPE html>";
+    this.htmlHeader = props.htmlHeader || '<!DOCTYPE html>';
     this.chunks = props.chunks || [];
     this.excludedChunks = props.excludedChunks || [];
     this.postRender = props.postRender || [];
     this.keepJsFile = props.keepJsFile || false;
   }
 
+  /**
+   * @param {*} compiler
+   */
   apply(compiler) {
-    compiler.hooks.thisCompilation.tap("react-to-static-html-webpack-plugin", (compilation) => {
-      compilation.hooks.additionalAssets.tapAsync("react-to-static-html-webpack-plugin", (doneOptimize) => {
+    compiler.hooks.thisCompilation.tap('react-to-static-html-webpack-plugin', compilation => {
+      compilation.hooks.additionalAssets.tapAsync('react-to-static-html-webpack-plugin', doneOptimize => {
+        const startDate = Date.now();
+        console.log('STARTED => ', startDate);
         const { assets, chunks } = compilation;
         const chunkPromises = this._compileChunk(chunks, assets, compilation);
 
-        Promise
-          .all(chunkPromises)
-          .then(() => doneOptimize())
+        Promise.all(chunkPromises)
+          .then(() => {
+            console.log('L => Finished in (ms): ', Date.now() - startDate);
+            
+            doneOptimize();
+          })
           .catch(ex => {
+            console.log('E => Finished in (ms): ', Date.now() - startDate);
             compilation.errors.push(ex);
             doneOptimize();
           });
@@ -32,47 +43,55 @@ module.exports = class ReactToHtmlWebpackPlugin {
   }
 
   _compileChunk(chunks, assets, compilation) {
-    return chunks.map(c => {
-      try {
-        if ((!this._hasChunks() || this._isChunksToWork(c.name)) && !this._isExcludedChunks(c.name)) {
-          return this._compileChunkSources(c, assets, compilation);
+    return chunks
+      .map(c => {
+        try {
+          if ((!this._hasChunks() || this._isChunksToWork(c.name)) && !this._isExcludedChunks(c.name)) {
+            return this._compileChunkSources(c, assets, compilation);
+          }
+        } catch (ex) {
+          compilation.errors.push(ex.stack);
         }
-      } catch (ex) {
-        compilation.errors.push(ex.stack);
-      }
 
-      return Promise.resolve();
-    }).reduce((p, n) => {
-      if (Array.isArray(n)) {
-        p = p.concat(n);
-      } else {
-        p.push(n);
-      }
+        return Promise.resolve();
+      })
+      .reduce((p, n) => {
+        if (Array.isArray(n)) {
+          p = p.concat(n);
+        } else {
+          p.push(n);
+        }
 
-      return p;
-    }, []);
+        return p;
+      }, []);
   }
 
   _compileChunkSources(chunk, assets, compilation) {
-    return chunk.files.filter(f => f.indexOf(`${chunk.name}.js`) >= 0).map(f => {
-      const renderedFilePromise = this._renderSource(f, assets[f].source());
+    return chunk.files
+      .filter(f => f.indexOf(`${chunk.name}.js`) >= 0)
+      .map(f => {
+        const renderedFilePromise = this._renderSourceIfNeed(f, assets[f].source(), chunk.contentHash.javascript);
 
-      renderedFilePromise.then(renderedFile => {
-        const fileName = this._parseAssetName(f);
+        renderedFilePromise.then(renderedFile => {
+          const fileName = this._parseAssetName(f);
 
-        compilation.assets[fileName] = this._parseRenderToAsset(renderedFile);
-        chunk.files.push(fileName);
-        chunk.files.splice(chunk.files.indexOf(f), 1);
+          compilation.assets[fileName] = this._parseRenderToAsset(renderedFile);
+          chunk.files.push(fileName);
+          chunk.files.splice(chunk.files.indexOf(f), 1);
 
-        if (!this.keepJsFile) {
-          delete compilation.assets[f];
-        }
+          if (!this.keepJsFile) {
+            delete compilation.assets[f];
+          }
 
-        return renderedFile;
+          return renderedFile;
+        });
+
+        return renderedFilePromise;
       });
+  }
 
-      return renderedFilePromise;
-    });
+  async _renderSourceIfNeed(assetName, source, hash) {
+    return getDataFromCacheOrMethod(assetName, hash, async () => await this._renderSource(assetName, source));
   }
 
   async _renderSource(assetName, source) {
@@ -90,27 +109,29 @@ module.exports = class ReactToHtmlWebpackPlugin {
 
     let elementPromise = Promise.resolve(element);
 
-    return elementPromise.then(element => {
-      if (!React.isValidElement(element)) {
-        element = React.createElement(element)
-      }
+    return elementPromise
+      .then(element => {
+        if (!React.isValidElement(element)) {
+          element = React.createElement(element);
+        }
 
-      let renderedFile = ReactDOMServer.renderToString(element);
+        let renderedFile = ReactDOMServer.renderToString(element);
 
-      if (renderedFile.trim().startsWith('<html')) {
-        renderedFile = `${this.htmlHeader}${renderedFile}`;
-      }
+        if (renderedFile.trim().startsWith('<html')) {
+          renderedFile = `${this.htmlHeader}${renderedFile}`;
+        }
 
-      this.postRender.forEach(f => {
-        renderedFile = f(renderedFile);
+        this.postRender.forEach(f => {
+          renderedFile = f(renderedFile);
+        });
+
+        return renderedFile;
+      })
+      .catch(ex => {
+        ex.message = `File ${assetName} gave an error: ${ex.message}`;
+
+        throw ex;
       });
-
-      return renderedFile;
-    }).catch(ex => {
-      ex.message = `File ${assetName} gave an error: ${ex.message}`
-
-      throw ex
-    });
   }
 
   _getGlobalsCopy() {
@@ -125,20 +146,11 @@ module.exports = class ReactToHtmlWebpackPlugin {
   _hadADefaultOrJustOneComponent(evaluatedSource) {
     const keys = Object.keys(evaluatedSource);
 
-    return (
-      evaluatedSource == null
-      || (
-        typeof (evaluatedSource.default) !== "function"
-        && (
-          keys.length > 1
-          || keys.length === 0
-        )
-      )
-    );
+    return evaluatedSource == null || (typeof evaluatedSource.default !== 'function' && (keys.length > 1 || keys.length === 0));
   }
 
   _parseAssetName(assetName) {
-    return `${assetName.replace(/\.[^/.]+$/, "")}.html`;
+    return `${assetName.replace(/\.[^/.]+$/, '')}.html`;
   }
 
   _parseRenderToAsset(render) {
@@ -148,7 +160,7 @@ module.exports = class ReactToHtmlWebpackPlugin {
       },
       size: () => {
         return render.length;
-      }
+      },
     };
   }
 
@@ -163,4 +175,4 @@ module.exports = class ReactToHtmlWebpackPlugin {
   _isExcludedChunks(chunkId) {
     return this.excludedChunks.some(c => c === chunkId);
   }
-}
+};
